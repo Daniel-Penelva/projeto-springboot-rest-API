66931d8b7fb68dfa6ff47f0070abcda810b629bd
Daniel Penelva <d4n.andrade@gmail.com>
2023-07-29 01:31:48 -0300

    Criando o projeto Spring Boot Rest API com Maven

---
+++ projeto-springboot-rest-api/src/main/java/br/com/springboot/projeto_springboot_rest_api/controllers/GreetingsController.java
@@ -0,0 +1,18 @@
+package br.com.springboot.projeto_springboot_rest_api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+}
